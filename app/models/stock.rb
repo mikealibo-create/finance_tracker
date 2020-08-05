a297d58f9a3909a4aaf34ade3ece7ad174ea594f
Mike <mike.alibo@bitcontroltech.com>
2020-08-05 16:48:33 +0800

    added secure credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,9 +2,9 @@ class Stock < ApplicationRecord
 
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
-            publishable_token: 'Tpk_03468247c8e044e29207ac6460766ccb',
-            secret_token: 'Tsk_86eb8e5d490a42ae84c6ab2563c899cc',
-            endpoint: 'https://sandbox.iexapis.com/v1'
+            publishable_token: Rails.application.credentials.iex[:tpk],
+            secret_token: Rails.application.credentials.iex[:tsk],
+            endpoint: Rails.application.credentials.iex[:ep]
         )
 
         client.price(ticker_symbol)
